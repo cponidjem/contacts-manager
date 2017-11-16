fc211a289877e4777e5c233bc111ceff2559d3da
ponidjem <cindy.ponidjem@outlook.fr>
2017-11-16 12:37:21 +0100

    Tests Must validés Tests Should validés Ajout d'une Interface Utilisateur en ligne de commande permettant d'utiliser l'annuaire de contacts (utiliser toutes les méthodes de la classe ContatcsManager) sans les test

--- src/main/java/com/fredericboisguerin/insa/ContactsManager.java
+++ src/main/java/com/fredericboisguerin/insa/ContactsManager.java
@@ -17,21 +17,30 @@ public class ContactsManager {
         Contact contact = new Contact(name, email, phoneNumber);
         listContacts.add(contact);
 
-
     }
 
     public void printAllContacts() {
+        int i = 0 ;
         for (Contact contact:listContacts) {
             System.out.println(contact.toString());
+            i++;
+        }
+        if (i==0) {
+            System.out.println("Pas de contact dans la liste");
         }
     }
 
     public void searchContactByName(String name) {
 
+        int i = 0 ;
         for (Contact contact:listContacts) {
             if (contact.name.toLowerCase().contains(name.toLowerCase())){
                 System.out.println(contact.toString());
+                i++;
             }
+        }
+        if (i==0){
+            System.out.println("Pas de contact pour ce nom");
         }
     }
 }
---
+++ src/main/java/com/fredericboisguerin/insa/UI.java
@@ -0,0 +1,62 @@
+package com.fredericboisguerin.insa;
+import java.util.Scanner;
+
+public class UI {
+
+    public static void main(String [] arguments){
+
+        ContactsManager contactsManager = new ContactsManager();
+
+        int choix;
+        Scanner sc = new Scanner(System.in);
+        do  {
+
+            System.out.println("\n Interface en ligne de commande");
+            System.out.println(
+                            "1- Afficher les contacts\n" +
+                            "2- Ajouter un contact\n" +
+                            "3- Chercher un contact\n" +
+                            "4- Quitter l'application\n");
+
+            try {
+                choix = Integer.parseInt(sc.nextLine());
+            }
+            catch (Exception e)
+            {
+                choix = 5;
+            }
+            switch (choix) {
+                case 1:
+                    contactsManager.printAllContacts();
+                    break;
+                case 2:
+                    System.out.println("Rentrez les informations du contact à ajouter :\n" + "Rentrez le nom :");
+                    String nom = sc.nextLine();
+                    System.out.println("Rentrez l'adresse mail :");
+                    String mail = sc.nextLine();
+                    System.out.println("Rentrez le numero de telephone :");
+                    String numero = sc.nextLine();
+
+                    try {
+                        contactsManager.addContact(nom, mail, numero);
+                    } catch (Exception e) {
+                        System.out.println("Exception levée par addcontact");
+                    }
+                    break;
+                case 3:
+                    System.out.println("Rentrez le nom du contact à rechercher :");
+                    nom = sc.nextLine();
+                    contactsManager.searchContactByName(nom);
+                    break;
+                case 4:
+                    System.out.println("Merci d'avoir utilisé le logiciel ! Bisous ! <3 <3\n");
+                    break;
+                default :
+                    System.out.println("idiot je veux un chiffre entre 1 et 4 !\n");
+                    break;
+            }
+        } while ((choix!=4));
+
+        sc.close();
+    }
+}
